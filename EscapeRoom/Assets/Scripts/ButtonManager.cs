5156ed235881177320cdd30994b5d4cd880a15c9
ffuszthaler <flo.fuszthaler@gmail.com>
2024-10-26 16:59:39 +0200

    feat: added button interactable

---
+++ EscapeRoom/Assets/Scripts/ButtonManager.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit.Interactors;
+
+public class ButtonManager : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+    
+    public void sayHello() 
+    {
+        Debug.Log("Hello");
+    }
+}
